3005e5e4d19f875ca9938c3a282c132bef6d1843
Angel L <angelruoqi@gmail.com>
2025-11-05 10:29:39 -0800

    Added notes-6-numbers

---
+++ notes-6-numbers.py
@@ -0,0 +1,23 @@
+# Numbers and Operations
+# Author: Angel Li
+# 5 November 2025
+
+# Work with numbers and operations
+
+# Create an algorithm to gather data to find the most popular bubble tea place around us
+
+# Version 1
+# Show all the choices
+# Ask the user for their choice
+# Keep track of a tally
+# Data analysis
+# Give the raw scores
+# Give scores as a percentage
+
+
+# Version 2
+# Ask the user to give their favourite bubble tea pplace
+# Keep track of a tally
+# Data analysis
+# Give the raw scores
+# Give scores as a percentage

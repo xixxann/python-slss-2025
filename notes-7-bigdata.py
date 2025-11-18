2442bc586704db628f22e85413b82372c8a9f554
Angel L <angelruoqi@gmail.com>
2025-11-18 09:45:28 -0800

    Added notes-7-bigdata

--- notes-7-bigdata.py
+++ notes-7-bigdata.py
@@ -11,12 +11,34 @@ def main():
     # Get the file
     path = "data/sfu_best_cmpt120.csv"
     file = open(path)
-    # Read the file
+    # Print the fist line of the file
     header_row = file.readline()
 
-    # Read the rest of the file
+    # Get information about the fave pizza place
+    uncle_fatihs = 0
+    club_ilia = 0
+    pizza_hut = 0
+
     for line in file:
-        print(line)
+        # fave pizza is in column 5 (4th index)
+        # a line is a string
+        # convert the string to a list
+        # split the line wherever a , is
+        info = line.split(",")
+        fave_pizza = info[4]
+
+        if fave_pizza == "Uncle Fatih's":
+            uncle_fatihs += 1
+        elif fave_pizza == "Club Ilia":
+            club_ilia += 1
+        elif fave_pizza == "Pizza Hut":
+            pizza_hut += 1
+
+    # Display results
+    print(f"Uncle Fatih's Votes: {uncle_fatihs}")
+    print(f"Club Ilia's Votes: {club_ilia}")
+    print(f"Pizza Hut's Votes: {pizza_hut}")
+
     file.close()
 
 

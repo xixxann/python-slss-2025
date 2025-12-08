e30c2fc209adcf37d4ac51417c3fe3e6d1659d51
Angel L <angelruoqi@gmail.com>
2025-12-08 14:57:54 -0800

    Added notes-9-sort

--- notes-9-sort.py
+++ notes-9-sort.py
@@ -2,7 +2,7 @@
 # Author: Angel Li
 # 4 December
 
-import csv
+import helper_spotify
 
 # Sorting Algorithms
 # The code below uses linear search to find songs from a particular artist
@@ -54,40 +54,83 @@ import csv
 #     return l
 
 
-# class example (Ver 2)
-def selection_sort(l: list[int], ascending=True) -> list[int]:
-    """Sorts a given list using selection sort
+# # class example (Ver 2)
+# def selection_sort(l: list[int], ascending=True) -> list[int]:
+#     """Sorts a given list using selection sort
+
+#     Params:
+#         l - list of nums to be sorted
+#         ascending - True if order is ascending
+
+#     Returns:
+#         a sorted list"""
+
+#     num_items = len(l)
+
+#     # start at the beginnng of list
+#     for i in range(num_items):
+#         candidate_num = l[i]
+#         candidate_index = i
+#         # check the rest of the list
+#         for j in range(i + 1, num_items):
+#             if ascending:
+#                 if l[j] < candidate_num:
+#                     candidate_num = l[j]
+#                     candidate_index = j
+#             else:
+#                 if l[j] > candidate_num:
+#                     candidate_num = l[j]
+#                     candidate_index = j
+#         # swap the current index with the lowest
+#         l[i], l[candidate_index] = l[candidate_index], l[i]
+#     return l
+
+
+def sort_songs(songs: list[list[str]], col: int, ascending=True) -> list[list[str]]:
+    """Sort a list of spotify songs in place
 
     Params:
-        l - list of nums to be sorted
-        ascending - True if order is ascending
+        songs - list of songs
+        col - column to sort
+        ascending - will sort ascending by default
 
-    Returns:
-        a sorted list"""
+    Returns: sorted list"""
+    # Use Selection Sort to sort songs
+    num_songs = len(songs)
+    # Starting at the beginning of the list
+    for i in range(num_songs):
+        # Set this value to the candidate's value
+        candidate_val = helper_spotify.string_to_num(songs[i][col])
+        candidate_idx = i
 
-    num_items = len(l)
-
-    # start at the beginnng of list
-    for i in range(num_items):
-        candidate_num = l[i]
-        candidate_index = i
-        # check the rest of the list
-        for j in range(i + 1, num_items):
+        for j in range(i + 1, num_songs):
+            this_songs_val = helper_spotify.string_to_num(songs[j][col])
             if ascending:
-                if l[j] < candidate_num:
-                    candidate_num = l[j]
-                    candidate_index = j
+                if this_songs_val < candidate_val:
+                    candidate_val = this_songs_val
+                    candidate_idx = j
             else:
-                if l[j] > candidate_num:
-                    candidate_num = l[j]
-                    candidate_index = j
-        # swap the current index with the lowest
-        l[i], l[candidate_index] = l[candidate_index], l[i]
-    return l
+                if this_songs_val > candidate_val:
+                    candidate_val = this_songs_val
+                    candidate_idx = j
+
+        # Swap!
+        songs[i], songs[candidate_idx] = songs[candidate_idx], songs[i]
+
+    return songs
 
 
 if __name__ == "__main__":
-    sorted_list = selection_sort([1, 43, 55, -11, 100, 34], False)
-    print(sorted_list)
-    sorted_list = selection_sort([1, 43, 55, -11, 100, 34], True)
-    print(sorted_list)
+    # Get all songs from Taylor Swift
+    taylors_songs = helper_spotify.songs_by_artist(
+        "data/spotify2024.csv", "Taylor Swift"
+    )
+    taylors_sorted_songs = sort_songs(taylors_songs, 11, ascending=False)
+
+    # Header on the results
+    print("Taylor Swift's Songs")
+    print("______________________")
+    print("Name\tYT Views")
+    # For every song, print out the Track Name, YT Views
+    for song in taylors_sorted_songs:
+        print(song[0], "\t", song[11])
